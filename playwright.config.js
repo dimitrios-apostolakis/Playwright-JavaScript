943d4d8ed4572c6d52c1b868b8b07c2575718f48
Dimitris Apostolakis <dsapostolakis@gmail.com>
2024-01-31 20:32:16 +0200

    Modify config settings

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
-// @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+import { defineConfig, devices } from "@playwright/test";
+import os from "os";
 
 /**
  * Read environment variables from file.
@@ -10,8 +10,8 @@ const { defineConfig, devices } = require('@playwright/test');
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-module.exports = defineConfig({
-  testDir: './tests',
+export default defineConfig({
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -19,25 +19,36 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : Math.ceil(os.cpus().length * 0.5),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // browserName: 'webkit',
     headless: true,
+    // serviceWorkers: "block",
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
+    // Capture screenshot after each test failure.
+    screenshot: "only-on-failure",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
+    // Record video only when retrying a test for the first time.
+    // video: 'retain-on-failure',
   },
-
+  /* Repeat each test a given number of times */
+  repeatEach: 1,
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "functional",
+      testDir: "./tests",
+      testMatch: /.*.spec.js/,
     },
 
     // {
@@ -51,10 +62,12 @@ module.exports = defineConfig({
     // },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
+    {
+      name: "mobile",
+      use: { ...devices["iPhone 12 Pro"] },
+      testDir: "./tests/mobile",
+      testMatch: /.*.spec.js/,
+    },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
@@ -78,4 +91,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
